327482eb850410eb8ed84b063ef695fbd2a20e03
XieMS <xms881126@gmail.com>
2023-03-03 23:03:39 +0800

    flutter test done

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Flutter_Test',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
@@ -27,22 +27,34 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var history = <WordPair>[];
+
+   GlobalKey? historyListKey; //  final GlobalKey historyListKey = GlobalKey();
 
     void getNext() {
-    current = WordPair.random();
-    notifyListeners();
+      history.insert(0, current); //history insert (index, element)
+      var animatedList = historyListKey?.currentState as AnimatedListState?;
+      animatedList?.insertItem(0);
+      current = WordPair.random();
+      notifyListeners();
   }
 
   var favorites = <WordPair>[];
 
-  void toggleFavorite() {
-      if (favorites.contains(current)) {
-        favorites.remove(current);
+  void toggleFavorite([WordPair? pair]) {
+    pair = pair ?? current;
+      if (favorites.contains(pair)) {
+        favorites.remove(pair);
       } else {
-        favorites.add(current);
+        favorites.add(pair);
       }
       notifyListeners();
     }
+
+  void remove(WordPair pair){
+    favorites.remove(pair); // pair = value (要刪掉的值)
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -55,54 +67,89 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+
     Widget page;
     switch (selectedIndex) {
       case 0:
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
+    // ----動畫 start-----
+    var homeArea = ColoredBox(
+      color: colorScheme.surfaceVariant,
+      child: AnimatedSwitcher(
+      duration: Duration(microseconds: 180),
+      child: page,),
+      );
+    //----動畫 end----
+
+    return Scaffold(
+      body:LayoutBuilder(
+        builder: (context, constraints) {
+
+          if(constraints.maxWidth < 450){
+            return Column(
+              children: [
+                Expanded(child: homeArea),
+                SafeArea(child: 
+                BottomNavigationBar(
+                  items: [
+                    BottomNavigationBarItem(
                       icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
+                      label: 'Home'),
+                    BottomNavigationBarItem(
                       icon: Icon(Icons.favorite),
-                      label: Text('Favorites'),
-                    ),
+                      label: 'Favorites'),
                   ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
+                  currentIndex: selectedIndex,
+                  onTap: (value) {
                     setState(() {
                       selectedIndex = value;
                     });
                   },
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
-              ),
+              )
             ],
-          ),
-        );
-      }
+          );
+        }else{
+          return Row(
+              children: [
+                SafeArea(
+                  child: NavigationRail(
+                    extended: constraints.maxWidth >= 600,
+                    destinations: [
+                      NavigationRailDestination(
+                        icon: Icon(Icons.home),
+                        label: Text('Home'),
+                      ),
+                      NavigationRailDestination(
+                        icon: Icon(Icons.favorite),
+                        label: Text('Favorites'),
+                      ),
+                    ],
+                    selectedIndex: selectedIndex,
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: homeArea,
+                ),
+              ],
+            );
+          }
+        }
+      )
     );
   }
 }
@@ -125,6 +172,11 @@ class GeneratorPage extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Expanded(
+            flex: 3,
+            child: HistoryListView(),
+          ),
+          SizedBox(height: 10),
           BigCard(pair: pair),
           SizedBox(height: 10),
           Row(
@@ -146,6 +198,7 @@ class GeneratorPage extends StatelessWidget {
               ),
             ],
           ),
+          Spacer(flex: 2)
         ],
       ),
     );
@@ -154,9 +207,9 @@ class GeneratorPage extends StatelessWidget {
 
 class BigCard extends StatelessWidget {
   const BigCard({
-    super.key,
+    Key? key,
     required this.pair,
-  });
+  }) : super(key: key);
 
   final WordPair pair;
 
@@ -171,19 +224,31 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: pair.asPascalCase,
+        child: AnimatedSize(
+         duration: Duration(microseconds: 180),
+         child: MergeSemantics(
+          child:Wrap(
+            children: [
+              Text(
+                pair.first,
+                style: style.copyWith(fontWeight: FontWeight.w200),
+              ),
+              Text(
+                pair.second,
+                style: style.copyWith(fontWeight: FontWeight.bold),
+              )
+            ],
+          ) ),
         ),
       ),
     );
   }
 }
 
-class Placeholder extends StatelessWidget {
+class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     var appState = context.watch<MyAppState>();
 
     if (appState.favorites.isEmpty) {
@@ -192,20 +257,96 @@ class Placeholder extends StatelessWidget {
       );
     }
 
-    return ListView(
+    return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text('You have '
               '${appState.favorites.length} favorites:'),
         ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
-    );
-  }
+        Expanded(
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 400,
+              childAspectRatio: 400 / 80
+              ),
+              children: [
+                for (var pair in appState.favorites)
+                ListTile(
+                  leading: IconButton(
+                    icon:Icon(Icons.delete_outline,semanticLabel: 'Delete'),
+                    color: theme.colorScheme.primary,
+                    onPressed: () {
+                      appState.remove(pair);
+                    },
+                    ),
+                     title: Text(
+                      pair.asLowerCase,
+                      semanticsLabel: pair.asPascalCase,
+                      ),
+                    ),
+                  ],
+                )
+              )
+            ],
+          );
+        }
 }
 
+class HistoryListView extends StatefulWidget {
+  const HistoryListView({Key? key}) : super(key: key);
+  @override
+  State<HistoryListView> createState() => _HistoryListViewState();
+}
+
+class _HistoryListViewState extends State<HistoryListView> {
+
+    final _key = GlobalKey();
+
+    static const Gradient _maskingGradient = LinearGradient(
+    colors: [Colors.transparent, Colors.black],
+    stops: [0.0, 0.5],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final appState = context.watch<MyAppState>(); //context.watch 取MyAppState值
+    appState.historyListKey = _key;
+
+    return ShaderMask(
+      shaderCallback: (bounds) => _maskingGradient.createShader(bounds), //漸變
+      blendMode: BlendMode.dstIn,
+      child: AnimatedList(
+        key: _key,
+        reverse: true,
+        padding: EdgeInsets.only(top: 100),
+        initialItemCount: appState.history.length,
+        itemBuilder: (context, index, animation){
+          final pair = appState.history[index];
+          return SizeTransition(
+            sizeFactor: animation, //通過獲取此值，放入child的尺寸值上
+            child: Center(
+              child: TextButton.icon(
+              onPressed: (){
+                appState.toggleFavorite(pair);
+              },
+              icon: appState.favorites.contains(pair)
+              ?Icon(Icons.favorite, size: 12)
+              :SizedBox(),
+              label: Text(
+                pair.asLowerCase,
+                semanticsLabel: pair.asPascalCase,
+              )
+            )
+            )
+          );
+        }
+      )
+    );
+  }
+
+
+}
